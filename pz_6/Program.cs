2d71e28b964f58bebd300fc6cd45ad5c8cd46be0
Mars <m.babiev05@mail.ru>
2022-09-29 10:43:13 +0500

    pz_6

---
+++ pz_6/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace pz_6
+{
+    internal class Program
+    {
+        /*
+         * 
+         */
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            int result = 1;
+            while (true) 
+            { 
+                result *= 2; 
+                if (result < n)
+                { 
+                    Console.WriteLine(result);
+                    ++i;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
