3381a755224759ad5baafc200aae9d91341a35b0
Mars <m.babiev05@mail.ru>
2022-10-02 18:59:16 +0500

    fix pz_6

--- pz_6/Program.cs
+++ pz_6/Program.cs
@@ -2,23 +2,18 @@
 {
     internal class Program
     {
-        /*
-         * 
-         */
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine());
-            int i = 0;
-            int result = 1;
-            while (true) 
-            { 
-                result *= 2; 
-                if (result < n)
-                { 
-                    Console.WriteLine(result);
-                    ++i;
+            int N = Convert.ToInt32(Console.ReadLine());
+            int pows = 1;
+            while (true)
+            {
+                pows *= 2;
+                if (pows < N)
+                {
+                    Console.WriteLine(pows);
                 }
-                else
+                else 
                 {
                     break;
                 }
