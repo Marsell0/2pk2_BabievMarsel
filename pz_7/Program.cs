304be0f990eac9a0d2e034ba56117e9840e9ffca
Mars <m.babiev05@mail.ru>
2022-10-06 10:22:33 +0500

    test

---
+++ pz_7/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace pz_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] arr = new int[12];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(-13, 13);
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
