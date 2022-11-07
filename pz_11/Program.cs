461db69de677f22a79bbb1d1f850d9c14f19c5c9
Mars <m.babiev05@mail.ru>
2022-11-07 11:48:18 +0500

    pz_11

---
+++ pz_11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+namespace pz_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int num;
+            string[] arrText = text.Split();
+            StringBuilder fin = new StringBuilder();
+            foreach (string i in arrText)
+            {
+                try 
+                {
+                    num = Convert.ToInt32(i);
+                    if (num > 9 || num < -9) 
+                    {
+                        fin.Append(Convert.ToString(i + " "));
+                    }
+                }
+                catch 
+                {
+                    num = 0;
+                }
+            }
+            Console.WriteLine(fin);
+        }
+    }
+}
