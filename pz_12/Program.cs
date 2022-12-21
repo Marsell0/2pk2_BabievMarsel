ebf6cf0493117914957aea0aa5ea46c503a734a2
Marsell0 <m.marsox0d@mail.ru>
2022-12-21 09:15:31 +0500

    pz_12 + pz_13

--- pz_12/Program.cs
+++ pz_12/Program.cs
@@ -1,14 +1,23 @@
-﻿namespace pz_12
+﻿using System.Security.Cryptography;
+
+namespace pz_12
 {
     internal class Program
     {
-        static void AreaOfParal(int f_a, int f_b, int f_c, int f_d, int s_a, int s_b, int s_c, int s_d,)
-        { 
-            
+        static void ParallelogramArea(int h1, int side1, int h2, int side2)
+        {
+            if (h1*side1 > h2 * side2)
+            {
+                Console.WriteLine($"Наибольший параллелограм под номером 1.\nЕго площадь - {h1*side1}");
+            }
+            else
+            {
+                Console.WriteLine($"Наибольший параллелограм под номером 2.\nЕго площадь - {h2 * side2}");
+            }
         }
         static void Main(string[] args)
         {
-            
+            ParallelogramArea(3, 4, 3, 6);
         }
     }
 }
---
+++ pz_13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace pz_13
+{
+    internal class Program
+    {
+        static int CountOf(string str, char subStr)
+        {
+            int count = 0;
+            foreach (char c in str)
+            {
+                if (c == subStr)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            string text = "hello world";
+            char subLine = 'l';
+            Console.WriteLine(CountOf(text, subLine));
+        }
+    }
+}
