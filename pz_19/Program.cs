d04cf51ac0a88010ccbb3b12f0a2a679596ecd68
Marsell0 <m.marsox0d@mail.ru>
2022-12-21 10:34:26 +0500

    pz_19

---
+++ pz_19/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+namespace pz_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\text.txt";
+            string pattern = @"\d{1} ([А-Яа-я]+ [А-Яа-я]{0,} [А-Яа-я]{0,}) ?\+";
+            Regex regex = new Regex(pattern);
+            using (StreamReader sr = new StreamReader(path))
+            {
+                foreach (Match match in regex.Matches(sr.ReadToEnd()))
+                {
+                    Console.WriteLine(match.Value);
+                }
+            }
+        }
+    }
+}
