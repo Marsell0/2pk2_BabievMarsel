4719fed19258b20768cf3153739d3914d163c6ae
Marsell0 <m.marsox0d@mail.ru>
2023-01-16 15:50:30 +0500

    pz_20

---
+++ pz_20/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace pz_20
+{
+    internal class Program
+    {
+        string gh = "werwer";
+        static string ReverseReg(string text)
+        {
+            string result = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsUpper(text[i]))
+                {   
+                    result += Char.ToLower(text[i]);
+                }
+                else if (Char.IsLower(text[i]))
+                {
+                    result += Char.ToUpper(text[i]);
+                }
+                else
+                {
+                    result += text[i];
+                }
+            }
+            try
+            {
+                Console.WriteLine(text[10]);
+            }catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Исключение обработано");
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseReg("Hel! lo"));
+        }
+    }
+}
