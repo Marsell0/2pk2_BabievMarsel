74a0ea925b71c17583a5bd2ab21950fafb2539cd
Marsell0 <m.marsox0d@mail.ru>
2023-01-28 13:19:09 +0500

    pz_21

---
+++ pz_21/Invoice.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_21
+{
+    internal class Invoice
+    {
+        public int ID { get; set; }
+        public string Date { get; set; }
+        public string Product { get; set; }
+        public int Count { get; set; }
+        public int Price { get; set; }
+        public double NDS { get; set; }
+        public Invoice(int iD, string date = "до 01.01.2000", string product, int count = 1, int price, int nDS = 20)
+        {
+            ID = iD;
+            Date = date;
+            Product = product;
+            Count = count;
+            Price = price;
+            NDS = nDS;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"ID накладной: {ID}\n" +
+                                $"Дата проведения накладной: {Date}\n" +
+                                $"Наименование продукта: {Product}\n" +
+                                $"Количество продукта: {Count}\n" +
+                                $"Цена за единицу продукта: {Price}\n" +
+                                $"НДС: {NDS}%");
+        }
+        public double GetFullPrice()
+        {
+            int price_without_nds = Price * Count;
+            double nds = NDS / 100;
+            double price_with_nds = price_without_nds + (nds * price_without_nds);
+            return price_with_nds;
+        }
+    }
+}
---
+++ pz_21/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace pz_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice first = new Invoice(1, "до 21.02.2022", "Сыр", 10, 135);
+            Invoice second = new Invoice(2, "до 21.06.2023", "Колбаса", 23, 240, 20);
+            Invoice third = new Invoice(3, "до 1.01.2022", "Хлеб", 10, 32, 5);
+            Invoice fourth = new Invoice(4, "до 24.12.2001", "Масло", 100, 50, 10);
+            first.PrintInfo();
+            Console.WriteLine(first.GetFullPrice());
+            Console.WriteLine();
+            second.PrintInfo();
+            Console.WriteLine(second.GetFullPrice());
+            Console.WriteLine();
+            third.PrintInfo();
+            Console.WriteLine(third.GetFullPrice());
+            Console.WriteLine();
+            fourth.PrintInfo();
+            Console.WriteLine(fourth.GetFullPrice());
+        }
+    }
+}
