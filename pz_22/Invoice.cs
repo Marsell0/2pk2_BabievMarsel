79d89c30f975f3d903832e733d2f64d714edaa32
Marsell0 <m.marsox0d@mail.ru>
2023-02-11 11:48:38 +0500

    pz_22 fin

--- pz_22/Invoice.cs
+++ pz_22/Invoice.cs
@@ -8,36 +8,55 @@ namespace pz_22
 {
     internal class Invoice
     {
-        public int ID { get; set; }
-        public string Date { get; set; }
-        public string Product { get; set; }
-        public int Count { get; set; }
-        public int Price { get; set; }
-        public double NDS { get; set; }
-        public Invoice(int iD, string date = "до 01.01.2000", string product, int count = 1, int price, int nDS = 20)
+        private DateTime _date;
+        static int id_counter = 1;
+        public int ID { get; set; } = id_counter; // убрал пользователю возможность устанвливать id через конструктор, чтобы использовать статический метод
+        public DateTime Date { 
+            get { return _date; } 
+            set
+            {
+                if (value.CompareTo(DateTime.Today) >= 0)
+                {
+                    _date = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Действие данной накладной закончилось. Установлена текущая дата.");
+                    _date = DateTime.Now;
+                }
+            }
+        }
+        public string Product { get; set; } = $"Продукт №{id_counter}";
+        public int Count { get; set; } = 1;
+        public int Price { get; set; } = 100;
+        public double NDS { get; set; } = 0.2;
+        public Invoice(DateTime date, string product, int count, int price, double nDS)
         {
-            ID = iD;
             Date = date;
             Product = product;
             Count = count;
             Price = price;
             NDS = nDS;
+            id_counter += 1;
         }
-
+        public Invoice()
+        {
+            Date = DateTime.Now;
+            id_counter += 1;
+        }
         public void PrintInfo()
         {
             Console.WriteLine($"ID накладной: {ID}\n" +
-                                $"Дата проведения накладной: {Date}\n" +
+                                $"Дата окончания накладной: {Date}\n" +
                                 $"Наименование продукта: {Product}\n" +
                                 $"Количество продукта: {Count}\n" +
                                 $"Цена за единицу продукта: {Price}\n" +
-                                $"НДС: {NDS}%");
+                                $"НДС: {NDS * 100}%");
         }
         public double GetFullPrice()
         {
             int price_without_nds = Price * Count;
-            double nds = NDS / 100;
-            double price_with_nds = price_without_nds + (nds * price_without_nds);
+            double price_with_nds = price_without_nds + (NDS * price_without_nds);
             return price_with_nds;
         }
     }
--- pz_22/Program.cs
+++ pz_22/Program.cs
@@ -4,21 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Invoice first = new Invoice(1, "до 21.02.2022", "Сыр", 10, 135);
-            Invoice second = new Invoice(2, "до 21.06.2023", "Колбаса", 23, 240, 20);
-            Invoice third = new Invoice(3, "до 1.01.2022", "Хлеб", 10, 32, 5);
-            Invoice fourth = new Invoice(4, "до 24.12.2001", "Масло", 100, 50, 10);
+            Invoice first = new Invoice(new DateTime(2025, 5, 15), "Сыр", 10, 135, 0.2);
             first.PrintInfo();
             Console.WriteLine(first.GetFullPrice());
             Console.WriteLine();
+            Invoice second = new Invoice();
             second.PrintInfo();
             Console.WriteLine(second.GetFullPrice());
-            Console.WriteLine();
-            third.PrintInfo();
-            Console.WriteLine(third.GetFullPrice());
-            Console.WriteLine();
-            fourth.PrintInfo();
-            Console.WriteLine(fourth.GetFullPrice());
         }
     }
 }
