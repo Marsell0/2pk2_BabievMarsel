3cdedb5eb26bb9bf653ad953503f9f76978e8401
Marsell0 <m.marsox0d@mail.ru>
2023-02-22 09:09:17 +0500

    pz_24

---
+++ pz_24/LandingInvoice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_24
+{
+    internal class LandingInvoice : Invoice
+    {
+        public string CourierFIO { get; set; }
+        public string MachineNumber { get; set; }
+        public LandingInvoice(DateTime date, string product, int count, int price, double nDS, string name, string number)
+        {
+            Date = date;
+            Product = product;
+            Count = count;
+            Price = price;
+            NDS = nDS;
+            CourierFIO = name;
+            MachineNumber = number;
+        }
+        public LandingInvoice(string name, string number) : base()
+        {
+            CourierFIO = name;
+            MachineNumber = number;
+        }
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"ФИО курьера: {CourierFIO}\n" +
+                                $"Гос. номер машины курьера: {MachineNumber}");
+            base.PrintInfo();
+
+        }
+        public object Clone()
+        {
+            return new LandingInvoice(Date, Product, Count, Price, NDS, CourierFIO, MachineNumber);
+        }
+    }
+}
---
+++ pz_24/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace pz_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice1 = new Invoice();
+            Invoice invoice2 = (Invoice)invoice1.Clone();
+            invoice2.Product = "сыр";
+            invoice1.PrintInfo();
+            Console.WriteLine();
+            invoice2.PrintInfo();
+
+            LandingInvoice landInv = new LandingInvoice("Ivanov Ivan Ivanovich", "с999сс");
+            LandingInvoice landInv2 = (LandingInvoice)landInv.Clone();
+            landInv2.CourierFIO = "Petrov Petr Petrovich";
+            landInv.PrintInfo();
+            Console.WriteLine();
+            landInv2.PrintInfo();
+        }
+    }
+}
