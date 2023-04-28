7afe8ab6608b04137218c184831080a7e7f8451d
Marsell0 <m.marsox0d@mail.ru>
2023-04-28 11:35:44 +0500

    fix pz_26

--- pz_26/MainWindow.xaml.cs
+++ pz_26/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +22,9 @@ namespace pz_26
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool isSaved = false;
+        bool isUnderline = false;
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -35,6 +40,93 @@ namespace pz_26
         {
             AboutWindow about = new AboutWindow();
             about.ShowDialog();
+        }
+
+        private void openFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Open);
+                FileInfo fileInfo = new FileInfo(fileStream.Name);
+                TextRange range = new TextRange(richTB.Document.ContentStart, richTB.Document.ContentEnd);
+                range.Load(fileStream, DataFormats.Rtf);
+                dateCreation.Text = Convert.ToString(fileInfo.LastAccessTime);
+                fileSize.Text = "размер: " + Convert.ToString(fileInfo.Length / 1024) + "кб";
+            }
+        }
+        private void saveFile(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Create);
+                FileInfo fileInfo = new FileInfo(fileStream.Name);
+                TextRange range = new TextRange(richTB.Document.ContentStart, richTB.Document.ContentEnd);
+                range.Save(fileStream, DataFormats.Rtf);
+                isSaved = true;
+                dateCreation.Text = Convert.ToString(fileInfo.LastAccessTime);
+                fileSize.Text = "размер: " + Convert.ToString(fileInfo.Length / 1024) + "кб";
+            }
+        }
+
+        private void UpdateCursorPosition(object sender, KeyEventArgs e)
+        {
+            TextPointer caretLineStart = richTB.CaretPosition.GetLineStartPosition(0);
+            TextPointer p = richTB.Document.ContentStart.GetLineStartPosition(0);
+            int currentLineNumber = 0;
+
+            while (true)
+            {
+                if (p == null || (caretLineStart != null && caretLineStart.CompareTo(p) < 0))
+                {
+                    break;
+                }
+
+                p = p.GetLineStartPosition(1, out var result);
+
+                if (result == 0)
+                {
+                    break;
+                }
+
+                currentLineNumber++;
+            }
+
+            position.Text = Convert.ToString(Math.Max(1, currentLineNumber));
+        }
+
+        private void makeItalic(object sender, RoutedEventArgs e)
+        {
+            if (richTB.Selection != null)
+            {
+                richTB.Selection.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Italic);
+            }
+            else
+                richTB.Selection.ApplyPropertyValue(TextElement.FontStyleProperty, null);
+        }
+
+        private void makeBold(object sender, RoutedEventArgs e)
+        {
+            if (richTB.Selection != null)
+            {
+                richTB.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
+            }
+            else
+                richTB.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, null);
+        }
+
+        private void makeUnderline(object sender, RoutedEventArgs e)
+        {
+            if (!isUnderline) 
+            {
+                richTB.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Underline);
+                isUnderline = true;
+            }
+            richTB.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, null);
+            isUnderline = false;
         }
     }
 }
