1bc4ea38be7917e22b46ade020c4d9d9c62f8c55
Marsell0 <m.marsox0d@mail.ru>
2023-05-11 15:17:40 +0500

    project uncomplete

---
+++ StudentCardLibrary/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace StudentCardLibrary
+{
+    public class Student
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Otchestvo { get; set; }
+        public DateOnly BDay { get; set; }
+        public string Group { get; set; }
+        public DateOnly ReceiptDate { get; set; }
+        public DateOnly RegistryDate { get; set; }
+
+        public void AddStudent()
+        {
+
+        }
+    }
+}
