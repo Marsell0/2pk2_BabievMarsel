41c8533596646827c5656e0f15fef105238ab148
Marsell0 <m.marsox0d@mail.ru>
2023-05-11 14:55:50 +0500

    pz28 uncomplete, pz29 uncomplete, started project

---
+++ pz_28/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_28
+{
+    internal class Customer
+    {
+        static int counter = 0;
+
+        public Customer()
+        {
+            counter++;
+        }
+    }
+}
